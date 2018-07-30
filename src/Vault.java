932b66b55260e5720e8d057c7ef4becdb5577c52
Isaacpants <dave@boyermoving.com>
2018-07-30 12:03:56 -0700

    Add files via upload

---
+++ src/Vault.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class Vault {
+	int sc;
+
+	public static void main(String[] args) {
+Vault v = new Vault();
+
+	}
+
+	Vault() {
+		
+	Random r = new Random();
+	
+	
+	
+	
+	
+sc = r.nextInt(1000000000);
+
+System.out.println(sc);
+	}
+	boolean tryCode(int num){
+	if(num==sc) {
+		return true;
+		
+	}else {
+		return false;
+	}
+		
+	}
+}
